4e50a8a03ccf1762a007cc11f49ba1fbdfb32ac4
jadiker <happylondon4@gmail.com>
2018-07-07 06:03:18 +0000

    updated license on html_helper

--- internal_code/html_helper.py
+++ internal_code/html_helper.py
@@ -1,12 +1,19 @@
 '''
-London Lowmanstone
+Copyright 2018 London Lowmanstone
+
+MIT License:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
 '''
 The goal of this file is to make it so I can import HTML files inside HTML files.
 It bothered be from a coding perspective to have to copy/paste HTML,
 ...and have to remember all the references in case I wanted to change something.
-So, this is a solution that allows me to generate HTML files with imports.
+So, this is a solution that allows me to generate HTML files from HTML files that import other HTML files.
 '''
 
 import re
@@ -26,9 +33,12 @@ def import_html_files(file_path, output_path=None, verbosity=2):
     ...and it will do all the substitutions in that (without saving that inner file)
     '''
     def default_output_path(input_path):
+        '''Take something that ends in ".htmlt.html" and return it so it ends in just ".html"'''
         return input_path[:-6]
         
-        
+    # a special file ending so that it doesn't run on everything
+    # it was going to just be ".htmlt" for "HTML Template", but then my editor didn't recognize that as an HTML file,
+    # ...so I made it the longer ".htmlt.html" so it was both special, but still recognized as an HTML file.
     file_ending = ".htmlt.html"
     assert file_path.endswith(file_ending)
     
@@ -118,8 +128,19 @@ def import_html_files(file_path, output_path=None, verbosity=2):
 
 
 if __name__ == "__main__":
+    '''
+    This is my usual workflow.
+    I create a directory called "internal_code" or something like that inside the root for my project.
+    The internal directory contains this script, and all of the ".htmlt.html" files as well as normal ".html" files.
+    My ".css" (and ".js") files go in the root folder itself, since that's where everything will be run from.
+    Then, I label the files that I actually want to display with the extension ".f.htmlt.html" where the ".f" stands for "final".
+    This script then looks for all of those files, performs the imports on them, and then saves them to the root folder.
+    This keeps the other imported ".htmlt.html" and ".html" files from cluttering up the root folder.
+    '''
+    
     import traceback
     for file_name in os.listdir("."):
+        # look particularly for ".htmlt.html" files that start with ".f" for "final"
         final_extension = ".f.htmlt.html"
         if file_name.endswith(final_extension):
             try:
